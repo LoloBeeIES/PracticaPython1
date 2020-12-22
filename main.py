fbb27b6e324838d10d34ac13bcf069234e2b5f66
Lolo_Bee <a20borhinm4021@ies-mardecadiz.com>
2020-12-22 17:14:18 +0100

    Added First Project

---
+++ main.py
@@ -0,0 +1,45 @@
+import pdb
+alumnos = []
+numPrimos = []
+
+numPares = 0
+
+_NUM_ALUMNOS=25
+
+def calcFactorial(numero):
+    res = 1
+    for i in range(1, numero+1):
+        print(i)
+        res *= i
+    return res
+
+def checkPrimo(numero):
+    if numero > 1:
+        for i in range(2, numero):
+            if (numero % i) == 0:
+                return False
+        else:
+            return numero
+
+print("Introduce la edad de " + str(_NUM_ALUMNOS) + " alumnos")
+while (len(alumnos)<_NUM_ALUMNOS):
+    try:
+        numero = int(input())
+    except:
+        print("El la edad introducida no es valida")
+        continue
+    alumnos.append(numero)
+
+alumnosSorted = sorted(alumnos)
+
+for edad in alumnosSorted:
+    if edad%2 == 0:
+        numPares+=1
+    if (checkPrimo(edad) != False):
+        numPrimos.append(edad)
+
+
+numPrimos = sorted(numPrimos)
+print("El mayor tiene %d y el menor %d" %(alumnosSorted[-1], alumnosSorted[0]))
+print("Edades pares %d Edades impares %d" %(numPares, len(alumnos)-numPares))
+print("El factorial del mayor (%d) es: %d" %(numPrimos[-1], calcFactorial(numPrimos[-1])))
